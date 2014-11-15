f77f7581dd7db2e850161d7f6e915eb9993b645b
yyypasserby <yyypasserby@yyypasserbydeMacBook-Pro.local>
2014-11-15 12:45:32 +0800

    add internet images

--- views/my_theatre_data.js
+++ views/my_theatre_data.js
@@ -6,9 +6,10 @@ app.controller('TheatrePresentCtrl', ['$scope', function($scope) {
     console.log('Threatre init!');
     $scope.theatre = { name : 'My Theatre' };
     $scope.video_list = [];
-    $scope.video_list.push({ thumbnail : '../imgs/1.jpg'});
-    $scope.video_list.push({ thumbnail : '../imgs/2.jpg'});
-    $scope.video_list.push({ thumbnail : '../imgs/3.jpg'});
-    $scope.video_list.push({ thumbnail : '../imgs/4.jpg'});
-    $scope.video_list.push({ thumbnail : '../imgs/5.jpg'});
+    $scope.video_list.push({ thumbnail : 'http://www.hackshanghai.com/img/logo-new.png'});
+    $scope.video_list.push({ thumbnail : 'http://www.hackshanghai.com/img/logo-new.png'});
+    $scope.video_list.push({ thumbnail : 'http://www.hackshanghai.com/img/logo-new.png'});
+    $scope.video_list.push({ thumbnail : 'http://www.hackshanghai.com/img/logo-new.png'});
+    $scope.video_list.push({ thumbnail : 'http://www.hackshanghai.com/img/logo-new.png'});
+
 }]);
